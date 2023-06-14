49ab3d552df6bbc9737f4ff10cd8adb72181c40b
chhue <chhue96@gmail.com>
2023-06-14 16:56:51 +0900

    네이버 맵 띄우기

--- app/src/main/java/com/ch96/centermap/view/MapActivity.kt
+++ app/src/main/java/com/ch96/centermap/view/MapActivity.kt
@@ -1,4 +1,18 @@
 package com.ch96.centermap.view
 
-class MapActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+
+import com.ch96.centermap.R
+import com.ch96.centermap.databinding.ActivityMapBinding
+import com.naver.maps.map.MapFragment
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = DataBindingUtil.setContentView<ActivityMapBinding>(this, R.layout.activity_map)
+
+    }
 }
--- app/src/main/java/com/ch96/centermap/view/SplashActivity.kt
+++ app/src/main/java/com/ch96/centermap/view/SplashActivity.kt
@@ -1,12 +1,25 @@
 package com.ch96.centermap.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.databinding.DataBindingUtil
 import com.ch96.centermap.R
+import com.ch96.centermap.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+
+        val binding = DataBindingUtil.setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
+        
+        // progressbar 2초 100% 로 수정
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MapActivity::class.java))
+        }, 2000)
+
     }
 }
